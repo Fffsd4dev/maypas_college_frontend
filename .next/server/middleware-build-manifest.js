ab96acee6ddad56306afadf5a6ab82b2b427b9c5
caryourdaysbm <adeyemioluwakayode73@gmail.com>
2025-09-22 14:25:36 +0100

    test done

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -15,11 +15,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/index.js"
     ],
-    "/404": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/404.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -29,16 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/course/[id]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/course/[id].js"
-    ],
-    "/courses": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/courses.js"
     ]
   },
   "ampFirstPages": []
--- components/slider/TestimonialSlider1.js
+++ components/slider/TestimonialSlider1.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import Slider from "react-slick"
+import { fetchTestimonials } from "@/util/testimonialApi"
 
 const settings1 = {
     slidesToShow: 1,
@@ -34,24 +35,44 @@ export default function TestimonialSlider1() {
     const [nav2, setNav2] = useState(null)
     const slider1Ref = useRef(null)
     const slider2Ref = useRef(null)
+    const [testimonials, setTestimonials] = useState([])
 
     useEffect(() => {
         setNav1(slider1Ref.current)
         setNav2(slider2Ref.current)
     }, [])
 
+    useEffect(() => {
+        fetchTestimonials()
+            .then(data => setTestimonials(Array.isArray(data) ? data : data.data || []))
+            .catch(() => setTestimonials([]))
+    }, [])
+
     return (
         <>
             <div className="row align-items-xl-center">
-                <div className="col-lg-5 col-md-7 col-sm-9">
+                  <div className="col-lg-5 col-md-7 col-sm-9">
                     <div className="testimonial__image-wrapper position-relative">
                         <Slider {...settings1} asNavFor={nav2} ref={slider1Ref} className="testimonial__image-active">
-                            <div className="testimonial__image-item">
-                                <img src="/assets/img/others/testimonial01.jpg" alt="img" />
-                            </div>
-                            <div className="testimonial__image-item">
-                                <img src="/assets/img/others/testimonial02.jpg" alt="img" />
-                            </div>
+                            {testimonials.length === 0 ? (
+                                <div className="testimonial__image-item">
+                                    <img src="/assets/img/others/testimonial01.jpg" alt="img" className="testimonial-img-fit" />
+                                </div>
+                            ) : (
+                                testimonials.map((t, idx) => (
+                                    <div className="testimonial__image-item" key={t.id || idx}>
+                                        <img
+                                            src={
+                                                t.student_photo
+                                                    ? process.env.NEXT_PUBLIC_API_BASE_URL + "/storage/" + t.student_photo
+                                                    : "/assets/img/others/testimonial01.jpg"
+                                            }
+                                            alt={t.student_name || "Student"}
+                                            className="testimonial-img-fit"
+                                        />
+                                    </div>
+                                ))
+                            )}
                         </Slider>
                         <div className="testimonial__shapes">
                             <img src="/assets/img/objects/testi_shape01.svg" alt="shape" data-aos="fade-up-left" data-aos-delay={300} />
@@ -74,36 +95,49 @@ export default function TestimonialSlider1() {
                             swipeToSlide={true}
                             focusOnSelect={true}
                             className="testimonial__content-active testimonial__content-nav">
-                            <div className="testimonial__content-item">
-                                <div className="testimonial__content-icon">
-                                    <img src="/assets/img/icons/quote.png" alt="img" />
+                            {testimonials.length === 0 ? (
+                                <div className="testimonial__content-item">
+                                    <div className="testimonial__content-icon">
+                                        <img src="/assets/img/icons/quote.png" alt="img" />
+                                    </div>
+                                    <p>No testimonials found.</p>
+                                    <div className="testimonial__content-avatar">
+                                        <h5 className="name">N/A</h5>
+                                        <span className="designation">Student</span>
+                                    </div>
                                 </div>
-                                <p>“ when an unknown printer took a galley of type and scrambled to make a type
-                                    specimen book. It has survived not only five centuries, but also the leap into
-                                    electronic.”</p>
-                                <div className="testimonial__content-avatar">
-                                    <h5 className="name">Parker Robert</h5>
-                                    <span className="designation">UI Designer</span>
-                                </div>
-                            </div>
-                            <div className="testimonial__content-item">
-                                <div className="testimonial__content-icon">
-                                    <img src="/assets/img/icons/quote.png" alt="img" />
-                                </div>
-                                <p>“ when an unknown printer took a galley of type and scrambled to make a type
-                                    specimen book. It has survived not only five centuries, but also the leap into
-                                    electronic.”</p>
-                                <div className="testimonial__content-avatar">
-                                    <h5 className="name">Parker Robert</h5>
-                                    <span className="designation">UI Designer</span>
-                                </div>
-                            </div>
+                            ) : (
+                                testimonials.map((t, idx) => (
+                                    <div className="testimonial__content-item" key={t.id || idx}>
+                                        <div className="testimonial__content-icon">
+                                            <img src="/assets/img/icons/quote.png" alt="img" />
+                                        </div>
+                                        <p>{t.content}</p>
+                                        <div className="testimonial__content-avatar">
+                                            <h5 className="name">{t.student_name}</h5>
+                                            <span className="designation">
+                                                {[1,2,3,4,5].map(n => (
+                                                    <i
+                                                        key={n}
+                                                        className="fas fa-star"
+                                                        style={{
+                                                            color: n <= Number(t.rating) ? "#FFD700" : "#ccc",
+                                                            marginRight: 2,
+                                                            fontSize: "1em"
+                                                        }}
+                                                    />
+                                                ))}
+                                            </span>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
                         </Slider>
                     </div>
                 </div>
             </div>
+            {/* Font Awesome CDN for star icons (if not globally loaded) */}
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
         </>
     )
-};
-
-
+}
